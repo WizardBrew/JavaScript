62c1b50557f779b2106ad73c37e923914da54e25
WizardBrew <wizardbrew84@gmail.com>
2023-09-29 00:58:27 +0530

    B_01Operators

---
+++ B_01Operators.js
@@ -0,0 +1,17 @@
+// Operators in JavaScript.
+
+let num = 10;
+let num1 = 20;
+let num2;
+num2 = num + num1;              // Cannot redefine as Let once defined just assign "num2 = "
+let num3 = num - num1;
+let num4 = num1 / num;
+let num5 = num * num1;
+let num6 = num5 % num4;
+
+
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
