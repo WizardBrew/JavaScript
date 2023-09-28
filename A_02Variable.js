c2aa7264dc15e9987070365f5c7e1bff5d1248f0
WizardBrew <wizardbrew84@gmail.com>
2023-09-29 00:39:50 +0530

    A_02Variable

---
+++ A_02Variable.js
@@ -0,0 +1,12 @@
+// The Variables and how to define;
+var name0 = "Parvez";
+var name1 = "Mustak";
+
+let profession = "IT Engineer";
+let age = 30;
+
+console.log("Type -- 1");       // Can use "+" or can be used "," to seprate from variable.
+console.log("i am " + name0 + " " + name1, "i am ", age, "I work as ", profession);
+console.log(`---------=============================----------`);
+console.log("Type -- 2");       // Using ` Tilda key
+console.log(`Hey, Hi!! i am, ${name0} ${name1} i am${age} working as${profession}`);
