da66c1543c25fd405829fe04cc5397910c65ebaa
WizardBrew <wizardbrew84@gmail.com>
2023-10-01 02:00:40 +0530

    practise

---
+++ ZH_01Functions.js
@@ -0,0 +1,22 @@
+console.log("functions")
+
+function greet(Uname, grtend = "Thanks") {
+        console.log(`Hi!!, ${Uname}   ${grtend}`)
+}
+
+let myname = "wizard";
+let lname = "Brew";
+
+greet(myname)
+
+function welcome(fname, bye = 'Visit again') {
+        console.log("Hi!!", fname);
+        let msg = `How are you you have been working good in Org \njust to let you know ${fname} we are putting you in dev team
+        congrats all the best ${bye}`
+        return msg
+}
+
+
+// let attribs  = welcome('Wiz')        // assign and call 
+// console.log(attribs)
+console.log(welcome(myname)) // direct call not recommended
